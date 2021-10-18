ff7f03b8203b4609213d458543f4204ba4f8626b
ShreyaSrivastava12 <91174445+ShreyaSrivastava12@users.noreply.github.com>
2021-10-18 22:49:56 +0530

    Update task5.cpp
    pattern

--- task5.cpp
+++ task5.cpp
@@ -2,21 +2,15 @@
 using namespace std;
 int main()
 {
-  int b[3][3];
-  int a [3] [3]={1,2,3,4,5,6,7,8,9};
-  for(int i=0;i<3;i++)
+  int i,j,n;
+  cout<<"enter number of lines";
+  cin>>n;
+  for(i =0;i<n;i++)
   {
-     for(int j=0;j<3;j++)
-     {
-       b[i][j]=a[2-i] [2-j];
-     }
-   }
-  for(int i=0;i<3;i++)
-  {
-     for(int j=0;j<3;j++)
-     {
-       cout<<b[i][j]<<" ";
-     }
+      for(j=0;j<=i;j++)
+      {
+        cout<<"*";
+      }
      cout<<endl;
    }
    return 0;
